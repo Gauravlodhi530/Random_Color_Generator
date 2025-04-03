5848c7b33e1f3b8353e9065b304ef7a5b9be29bc
Gaurav Kumar <114479314+Gauravlodhi530@users.noreply.github.com>
2025-04-03 17:53:49 +0530

    Delete p.js

--- p.js
+++
@@ -1,7 +0,0 @@
-const getcolor = () => {
-    const randomnymber = Math.floor(Math.random() * 16777215);
-    const hex = '#' + randomnymber.toString(16);
-    console.log(hex);
-}
-
-document.querySelector('.btn').addEventListener('click', getcolor);
